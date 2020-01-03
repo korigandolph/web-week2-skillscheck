81c835a1508e81e3a0c6e86a2f0fe20ccd94a722
Kori Gandolph <korigandolph@Koris-MacBook-Air.local>
2020-01-03 12:44:32 -0700

    aybe done

--- javascript-1/practice.js
+++ javascript-1/practice.js
@@ -2,42 +2,64 @@
 
 // Create a variable called 'myName' with a value that is your name. 
 
+let myName = 'kori'
 // Code here
 
 //////////////////PROBLEM 2////////////////////
 
 // Create a variable called 'faveNum' with a value that is your favorite number. 
 
+let faveNum = 17
 //Code here
 
 //////////////////PROBLEM 3////////////////////
 
 // Create a variable called 'lovesCode' and assign it a value of true.
 
+let lovesCode = true
 //Code here
 
 //////////////////PROBLEM 4////////////////////
 
 // Create a function called 'sum' that takes in two parameters, 'num1' and 'num2'. The function should return the sum of the two parameters.
 
+function sum(num1, num2){
+	return num1 + num2
+}
 //Code here
 
 //////////////////PROBLEM 5////////////////////
 
 // Create a function called 'lovesCodeChecker' that takes in a single parameter called 'x'. Check to see if 'x' is equal to true. If it is, return a true boolean. If 'x' does not equal true, return a false boolean.  
 
+function lovesCodeChecker(x){
+	if(x){
+		return true;
+	} else{
+		return false;
+	}
+}
 //Code here
 
 //////////////////PROBLEM 6////////////////////
 
 // Create a function called 'oddChecker' that takes in one parameter, 'num'. Your function should check to see if the parameter is even or odd. If the parameter is odd, return 'the number is odd'. If the number is even, return 'the number is even'.
 
-//Code here
+function oddChecker (num){
+	if(num %2 === 0){
+		return 'the number is even';
+	} else {
+		return 'the number is odd';
+	}
+}
 
 //////////////////PROBLEM 7////////////////////
 
 // Create a function called 'iLove' that takes in two string parameters, 'name' and 'love'. Have the function take the two parameters and return a string that says "NAMEPARAM loves LOVEPARAM" with the appropriate parameters in the string. e.g. "Joseph loves music"
 
+function iLove(name, love){
+	return `${name} loves ${love}`
+}
 //Code Here
 
 //////////////////PROBLEM 8////////////////////
@@ -45,12 +67,14 @@
 // Create a copy of the faveColors array called 'colorCopy' using slice.
 const faveColors = ['red', 'green', 'black']
 
+let colorCopy = faveColors.slice()
 //Code Here
 
 //////////////////PROBLEM 9////////////////////
 
 // Add a fourth color to the end of the 'colorCopy' array using push.
 
+colorCopy.push('color')
 //Code Here
 
 //////////////////PROBLEM 10////////////////////
@@ -58,24 +82,52 @@ const faveColors = ['red', 'green', 'black']
 // Create a new array called 'middleNums' from the numbers array that will capture only the middle numbers (2, 3, 4).
 const numbers = [1, 2, 3, 4, 5]
 
+var middleNums = numbers.slice(1,4)
 //Code Here
 
 //////////////////PROBLEM 11////////////////////
 
 // Create an object called 'me' that has the following keys: firstName, state, age, and greeter. The value of the firstName key should be your name as a string. The value of the property state should be your current state or providence of residence as a string. The value of age should be your age as a number. greeter should be a method that returns the string 'Hello! My name is NAMEVALUE and I live in STATEVALUE' with the corresponding values. 
 
+let me = {
+	firstName: 'kori',
+	state: 'utah',
+	age: 22,
+	greeter: function(){
+		return 'Hello! My name is ${me.firstName} and I live in ${me.state}'
+}
+}
 //Code Here
 
 //////////////////PROBLEM 12////////////////////
 
 // Create a function called 'bigOrSmall' that takes in one parameter, 'arr', which will be an array of numbers. Inside of the bigOrSmall function, create a new array called 'answers'. Then, loop over the passed in arr parameter, and check to see if the number in the array is GREATER than 100. If it is, push 'big' as a string to the answers array. If the number is LESS than or EQUAL to 100, push 'small' as a string to the answers array. Return the answers array inside of the function.
 
+function bigOrSmall(arr){
+	let answers = []
+	arr.forEach(x =>{
+		if(x > 100){
+			answers.push('big')
+		}
+		if(x<= 100){
+			answers.push('small')
+		}
+	})
+	return answers
+	}
 // Code here
 
 //////////////////PROBLEM 13////////////////////
 
 // Create a function called 'arrayReverser' that takes in one parameter, 'arr'. Inside of arrayReverser, create an empty array called 'reversed'. Using a for loop, loop over the passed in array IN REVERSE (this means your counter should decrement), and then add each item to the 'reversed' array variable you created. Finally, return the 'reversed' array variable. 
 
+function arrayReverser(arr){
+	let reversed= []
+	for(let i=arr.length-1; i>=0; i--){
+		reversed.push(arr[i])
+	}
+	return reversed
+}
 //Code Here
 
 //////////////////PROBLEM 14 - 18 Setup////////////////////
@@ -88,23 +140,29 @@ const myNumbers = [3, 55, 788, 2, 1]
 
 // First, use .map to create a new array that doubles each number. Call the new array 'doubled'.
 
+let doubled = myNumbers.map(x=> x*2)
 // Code Here
 
 //////////////////PROBLEM 15////////////////////
 
 // Now, use .filter to create a new array containing any numbers that are greater than 100. Call the new array 'filtered'. 
 
+let filtered = myNumbers.filter(x=> x > 100)
 // Code Here
 
 //////////////////PROBLEM 16////////////////////
 
 // Next, use reduce to get the total of the numbers in the array summed together. Store the total in a new variable named 'total'. 
 
+let total = myNumbers.reduce((tot, curr) => tot + curr)
 // Code Here
 
 //////////////////PROBLEM 17////////////////////
 
 // Finally, use .forEach to find the index of each item in the array. To begin, create an empty array called 'myNumbersIndex'. Then, use forEach to push each item's index from the myNumbers array into the newly created myNumbersIndex array. 
+
+let myNumbersIndex = []
+myNumbers.forEach((element, index) => myNumbersIndex.push(index))
 
 // Code Here
 
@@ -113,6 +171,7 @@ const myNumbers = [3, 55, 788, 2, 1]
 // Did you know that George Foreman has five sons named George? Go ahead and change everyone's name in the notGeorge array to George using .map. Store the resulting array in a variable named 'forTheLoveOfGeorge'.
 const notGeorge = ['Louis', 'Ted', 'Bill', 'Sharon', 'Mark', 'Angela']
 
+let forTheLoveOfGeorge = notGeorge.map(x => 'George')
 // Code Here
 
 //////////////////PROBLEM 19////////////////////
@@ -127,10 +186,12 @@ const people = [
 	{ name: 'Josh', friend: true, awesomeLevel: 7 }
 ]
 
+let enemies = people.filter(x=> x.friend===false)
 // Code Here
 
 //////////////////PROBLEM 20////////////////////
 
 // Using the people array above, get a total of the awesomeLevel from all the people, and store that value in a variable named 'totallyAwesome'. Use .reduce().
 
+let totallyAwesome = people.map(x => x.awesomeLevel).reduce((acc, curr) => acc + curr)
 // Code Here
--- javascript-4/student.js
+++ javascript-4/student.js
@@ -4,6 +4,7 @@
 //////////////////Step 1////////////////////
 // Create a new array called 'faveColors' and assign it a value of your three favorite colors as strings.
 
+const faveColors = ['black', 'white', 'grey']
 // CODE HERE
 
 //////////////////Step 2////////////////////
@@ -20,12 +21,24 @@
 //  The superPowerXP key value should be a method that returns a random number between 1-100. Use Math.floor(Math.random() * 100) + 1 
 
 // The profileImage key value should be a method that returns this url `https://randomuser.me/api/portraits/med/lego/${Math.floor(Math.random() * 10) + 1 }.jpg`.
-
+let me = {
+    firstName: 'Kori',
+    superHeroName: 'Just Kori',
+    homeTown: 'Alpine',
+    superPowers: ['mind control', 'flying', 'teleporting'], 
+    superPowerXP: function(){
+        return Math.floor(Math.random() * 100) + 1 
+    },
+    profileImage: function(){
+        return `https://randomuser.me/api/portraits/med/lego/${Math.floor(Math.random() * 10) + 1 }.jpg`
+    }
+}
 // CODE HERE
 
 //////////////////Step 3////////////////////
 // In this step, you will create three variables that will hold some data from your me object. The first variable you create should be 'regularName', and it should receive its value from the value of your firstName key on the me object above. The next variable is 'superName' which should have its value assigned as the value of the superHeroName property on the me object. The last variable you should create is 'homeTown' which will be the value of homeTown on the me object
 
+let {firstName: regularName, superHeroName: superName, homeTown}= me
 // CODE HERE
 
 //////////////////Step 4////////////////////
@@ -33,15 +46,33 @@
 
 // Next, create a for loop to loop over the remaining three colors. If any of the colors is 'blue', change it's value to '#4D4DFF' (which is just a more appealing shade of blue). Outside of the for loop but still inside of setColor, invoke the function called 'background' which will take in three arguments. These arguments should be the three items remaining in your colors array. 
 
+function setColor(arr){
+    arr.splice(3,0)
+    for(let i=0; i< arr.length; i++){
+        if(arr[i] == 'blue'){
+            arr[i] = '#4D4FF'
+        }
+    } background(arr[0], arr[1], arr[2])
+}
 // CODE HERE
 
 //////////////////Step 5////////////////////
 //Create a function called 'setPowers' that takes in 'arr' as a parameter. In your setPowers function, loop over the arr parameter and run a function we created called createLi(), which will take each item of the array as an argument. Remember, you did not create the createLi function. The createLi function is a function we created that determines how the content is displayed.
 
+function setPowers(arr){
+    for(let i=0; i<arr.length; i++){
+        createLi(arr[i])
+    }
+}
 // CODE HERE
 
 
 //////////////////Step 6////////////////////
 //Lastly, create a function called 'redactInfo' that will take in an object called 'obj' as a parameter. Now, imagine your super hero needs to go undercover, and you need to remove all info about them. That is what this function will do. In your function, loop over the obj parameter passed in, and change each keys value to 'redacted'. Outside of the loop but still in the redactInfo function, run the function redacted() which is a function we created that will update the text on the screen.
 
+function redactInfo(obj){
+    for(let key in obj){
+        obj[key] = 'redacted'
+    }redacted()
+}
 // CODE HERE
